acf2398c70c14798df0b82b38c2d5273bbc1ce8b
jdrain <jdrain@email.sc.edu>
2017-03-09 12:29:25 -0500

    test_script now allows the user to determine the size of the board and the length of the game themself

--- test_script.py
+++ test_script.py
@@ -1,12 +1,17 @@
 from board import board
 from game import game
 import time
+import sys
 
-b=board(50)
-b.randomize()
-print("Randomized board: ")
-b.display()
-time.sleep(.5)
-g=game(500,b,[])
-g.construct_game()
-g.display()
+#get user args for board size and game length
+if len(sys.argv) != 3:
+    print("use is of the form: python test_script.py <board size> <game length>\n")
+else:
+    b=board(int(sys.argv[1]))
+    b.randomize()
+    print("Randomized board: ")
+    b.display()
+    time.sleep(.5)
+    g=game(int(sys.argv[2]),b,[])
+    g.construct_game()
+    g.display()
